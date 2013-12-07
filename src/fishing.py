cdf31487b5527b4daff22dddf80d00d639ba23a3
cetoli <carlo@nce.ufrj.br>
2013-12-07 07:51:11 -0200

    feat (fishing): Add box, sphere ellipsoid

--- src/fishing.py
+++ src/fishing.py
@@ -14,38 +14,82 @@ Fishing is a photo safari in the deeps of the ocean.
 """
 __version__ = '0.1.0'
 
+SCENE = None
+
 
 class Aquarium:
     def __init__(self, bry, _cons):
-        self.bry = bry
+        global SCENE
+        self.bry, self._cons, self._thr = bry, _cons, bry.THR
         #print(bry, dir(bry))
-        cameraC = _cons(bry.THR.PerspectiveCamera)
+        cameraC = _cons(self._thr.PerspectiveCamera)
         self.camera = cameraC(75, 1, 1, 10000)
         self.camera.position.z = 1000
 
-        sceneC = _cons(bry.THR.Scene)
-        self.scene = sceneC()
+        sceneC = _cons(self._thr.Scene)
+        SCENE = self.scene = sceneC()
 
-        geometry = _cons(bry.THR.CubeGeometry)(200, 200, 200)
-        material = _cons(bry.THR.MeshBasicMaterial)({"color": "#FF0000", "wireframe": True})
-        geometry = _cons(bry.THR.SphereGeometry)(200, 15, 15)
-        a = _cons(bry.THR.Vector3)(1, 1.2, 1.5)
-        scale = _cons(bry.THR.Matrix4)().makeScale(a)
-        geometry.applyMatrix(scale)
-        material = _cons(bry.THR.MeshBasicMaterial)({"color": "#FF0000", "wireframe": True})
+        geometry = _cons(self._thr.CubeGeometry)(200, 200, 200)
+        material = _cons(self._thr.MeshBasicMaterial)({"color": "#FF0000", "wireframe": True})
+        geometry = _cons(self._thr.SphereGeometry)(200, 20, 20)
+        svect = _cons(self._thr.Vector3)(1, 1.2, 1.5)
+        #svect = _cons(self._thr.Vector3)(1, 1, 1)
+        scale = _cons(self._thr.Matrix4)().makeScale(1, 1.2, 1.5)
+        #geometry.applyMatrix(scale)
+        material = _cons(self._thr.MeshBasicMaterial)({"color": "#FF0000", "wireframe": False})
+        self.box(pos=(300, -100, 200))
+        self.ellipsoid(pos=(-300, 100, 200), length=300, width=170, height=150, color='#FFFF00', opacity=0.5)
 
-        meshC = _cons(bry.THR.Mesh)
+        meshC = _cons(self._thr.Mesh)
         self.mesh = meshC(geometry, material)
         self.scene.add(self.mesh)
-
-        rendererC = _cons(bry.THR.CanvasRenderer)
+        self.mesh.scale = svect
+        rendererC = _cons(self._thr.CanvasRenderer)
         self.renderer = rendererC()
         self.renderer.setSize(444, 444)
         bry.DOC['main'].appendChild(self.renderer.domElement)
+        bry.DOC['main'].style.background = "black"
         self.renderer.render(self.scene, self.camera)
 
+    def frame(self):
+        return self._cons(self._thr.Object3D)()
+
+    def box(self, pos=(0, 0, 0), length=100, width=100, height=100,
+            color="#000000", opacity=1, wire = False, axis=(0, 0, 0), frame=SCENE):
+        geometry = self._cons(self._thr.CubeGeometry)(length, width, height)
+        material = self._cons(self._thr.MeshNormalMaterial)({"color": color, "wireframe": wire, "opacity": opacity})
+        element = self._cons(self._thr.Mesh)(geometry, material)
+        element.position.x, element.position.y, element.position.z = pos
+        element.rotation.x, element.rotation.y, element.rotation.z = axis
+        frame = frame or self.scene
+        frame.add(element)
+        return element
+
+    def sphere(self, pos=(0, 0, 0), length=100, hor=20, vert=20,
+               color="#000000", opacity=1, wire = False, axis=(1, 0, 0), frame=SCENE):
+        geometry = self._cons(self._thr.SphereGeometry)(length, hor, vert)
+        material = self._cons(self._thr.MeshBasicMaterial)({"color": color, "wireframe": wire, "opacity": opacity})
+        element = self._cons(self._thr.Mesh)(geometry, material)
+        element.position.x, element.position.y, element.position.z = pos
+        element.rotation.x, element.rotation.y, element.rotation.z = axis
+        frame = frame or self.scene
+        frame.add(element)
+        return element
+
+    def ellipsoid(self, pos=(0, 0, 0), length=100, width=20, height=20, hor=20, vert=20,
+                  color="#000000", opacity=1, wire = False, axis=(1, 0, 0), frame=SCENE):
+        geometry = self._cons(self._thr.SphereGeometry)(length/2, hor, vert)
+        material = self._cons(self._thr.MeshBasicMaterial)({"color": color, "wireframe": wire, "opacity": opacity})
+        element = self._cons(self._thr.Mesh)(geometry, material)
+        element.position.x, element.position.y, element.position.z = pos
+        element.rotation.x, element.rotation.y, element.rotation.z = axis
+        element.scale = self._cons(self._thr.Vector3)(1, width/length, height/length)
+        frame = frame or self.scene
+        frame.add(element)
+        return element
+
     def animate(self, i):
-        # note: bry.THR.js includes requestAnimationFrame shim
+        # note: self._thr.js includes requestAnimationFrame shim
         self.bry.WIN.requestAnimationFrame(self.animate)
 
         self.mesh.rotation.x += 0.01
