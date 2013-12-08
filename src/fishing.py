e5dd0f510cd614917b919764d438bc2da545f5c9
cetoli <carlo@nce.ufrj.br>
2013-12-08 13:05:30 -0200

    feat (fishing): Add wglRenderer

--- src/fishing.py
+++ src/fishing.py
@@ -71,13 +71,26 @@ class WGLVisual:
     def __init__(self, bry, _cons):
         global SCENE
         self.bry, self._cons, self._thr = bry, _cons, bry.THR
+
+        SCREEN_WIDTH = bry.WIN.innerWidth
+        SCREEN_HEIGHT = bry.WIN.innerHeight
+        VIEW_ANGLE = 45
+        ASPECT = 1  # SCREEN_WIDTH / SCREEN_HEIGHT
+        NEAR = 1  # 0.1
+        FAR = 10000
         self.camera = _cons(self._thr.PerspectiveCamera)(75, 1, 1, 10000)
+        #self.camera = _cons(self._thr.PerspectiveCamera)(VIEW_ANGLE, ASPECT, NEAR, FAR)
         self.camera.position.z = 1000
+        #self.camera.position.set(0, 150, 1000)
+
         SCENE = self.scene = _cons(self._thr.Scene)()
+        #self.camera.lookAt(self.scene.position)
         self.renderer = _cons(self._thr.CanvasRenderer)()
+        #self.renderer = _cons(self._thr.WebGLRenderer)({"antialias": True})
         self.renderer.setSize(444, 444)
         bry.DOC['main'].appendChild(self.renderer.domElement)
-        bry.DOC['main'].style.background = "black"
+        #bry.DOC['main'].style.background = "black"
+        self.draw()
         self.renderer.render(self.scene, self.camera)
 
     def draw(self):
@@ -92,17 +105,27 @@ class WGLVisual:
     def wglelement(self, geometry, pos=(0, 0, 0), sx=1.0, sy=1.0, sz=1.0, size=None, offrz=0.0,
                    color="#FFFFFF", opacity=1.0, wire = False, axis=(0, 0, 0), frame=SCENE):
         material = self._cons(self._thr.MeshBasicMaterial)({"color": color, "wireframe": wire, "opacity": opacity})
+        #material = self._cons(self._thr.MeshPhongMaterial)(dict(
+        #    ambient='#660000', color='#ff0000',
+        #    specular='#ff8080', shininess=50))
+        #material = self._cons(self._thr.MeshPhongMaterial)(dict(color=100, shininess=50))
+        #material = self._cons(self._thr.MeshLambertMaterial)()  # ({"color": "rgb(250, 0,0)"})
+        material.color = self._cons(self._thr.Color)(color)  # "#FF0000"
+
         element = self._cons(self._thr.Mesh)(geometry, material)
 
         def posit(x=0, y=0, z=0):
             element.position.x, element.position.y, element.position.z = int(x), int(y), int(z)
         posit(*pos)
         raxis = lambda x, y, z: (at2(y, z), at2(x, z)+offrz, at2(x, y))
-        element.scale = self._cons(self._thr.Vector3)(sx, sy, sz)
         element.rotation.x, element.rotation.y, element.rotation.z = raxis(*axis)
+        element.scale = self._cons(self._thr.Vector3)(sx, sy, sz)
         frame = frame or self.scene
         frame.add(element)
         return element
+
+    def vect(self, x, y, z):
+        return self._cons(self._thr.Vector3)(x, y, z)
 
     def box(self, length=100, width=100, height=100, **kwargs):
         geometry = self._cons(self._thr.CubeGeometry)(length, width, height)
@@ -114,7 +137,27 @@ class WGLVisual:
 
     def pyramid(self, size=(100, 100, 100), base=4, top=0, **kwargs):
         geometry = self._cons(self._thr.CylinderGeometry)(top, size[0]/sqrt(2), size[1], base, false)
-        return self.wglelement(geometry, sz=size[2]/size[0], offrz=pi/4, **kwargs)
+        aspect = size[2]/size[0]
+        return self.wglelement(geometry, sx=aspect, sz=1/aspect, offrz=pi/4, **kwargs)
+
+    def npyramid(self, size=(100, 100, 100), base=4, top=0, **kwargs):
+        a, b, c = size
+        points = [
+            self.vect(a/2, b/2, 0),
+            self.vect(-a/2, -b/2, 0),
+            self.vect(-a/2, b/2, 0),
+            self.vect(a/2, -b/2, 0),
+            self.vect(0, 0, c)
+        ]
+        shape = geometry = self._cons(self._thr.ShapeGeometry)()
+        shape.moveTo(a/2, b/2, 0)
+        shape.moveTo(-a/2, -b/2, 0)
+        shape.moveTo(-a/2, b/2, 0)
+        shape.moveTo(a/2, -b/2, 0)
+        shape.moveTo(0, 0, c)
+        shape.close()
+        aspect = size[2]/size[0]
+        return self.wglelement(geometry, **kwargs)
 
     def cone(self, radius=50, height=100, base=RES, top=0, **kwargs):
         geometry = self._cons(self._thr.CylinderGeometry)(top, radius, height, base, false)
@@ -197,7 +240,15 @@ class Peixe(WGLVisual):
 class Aquarium(WGLVisual):
     def __init__(self, bry, _cons):
         WGLVisual.__init__(self, bry, _cons)
+
+    def draw(self):
         frame = self.frame()
+        floorMaterial = self._cons(self._thr.MeshBasicMaterial)({"color": Color.yellow, "wireframe": False})
+        floorGeometry = self._cons(self._thr.PlaneGeometry)(10000, 10000, 10, 10)
+        floor = self._cons(self._thr.Mesh)(floorGeometry, floorMaterial)
+        #floor.position.y = -1
+        floor.rotation.x = pi / 2
+        self.scene.add(floor)
         self.box(frame=frame, pos=(300, -200, 200), length=200, width=200, height=100, color= Color.red)
         self.pyramid(frame=frame, pos=(300, 0, 200), size=(200, 200, 100), color= Color.cyan)
         self.ring(frame=frame, pos=(-100, -300, 200))
@@ -219,4 +270,5 @@ class Aquarium(WGLVisual):
 def main(bry, _cons):
     print('Fishing %s' % __version__)
     Aquarium(bry, _cons).animate(0)
+    #Aquarium(bry, _cons)
     #Peixe(bry, _cons).animate(0)
